6c96261e1ded23567aa089b3cf718001f66714db
Fountainhead43 <zdcox@uark.edu>
2016-11-22 00:31:09 -0600

    Fixed dead links

--- signup.php
+++ signup.php
@@ -36,8 +36,8 @@
                 </div>
                 <div class="navbar-collapse collapse" id="nav-bar-target">
                     <ul class="nav navbar-nav navbar-right">
-                        <li class="about"><a href="about.html">About</a></li>
-                        <li class="login"><a href="login.html">Log In</a></li>
+                        <li class="about"><a href="about.php">About</a></li>
+                        <li class="login"><a href="login.php">Log In</a></li>
                         <li class="about"><a href="logout.php">Log-out</a></li>
                     </ul>
                 </div>
