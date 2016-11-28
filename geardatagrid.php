a1e0a03474485aff90e848712aaaf07e91858c90
Russell-PC\Russell <russelljacobs111@icloud.com>
2016-11-27 22:45:43 -0600

    Miscellaneous fixes
    Also testing travis build

--- geardatagrid.php
+++ geardatagrid.php
@@ -1,22 +1,9 @@
 <?php
 #Include the dbh.php file
 include ('dbh.php');
-// Connect to the database
-$pdo = new PDO("mysql:host=$hostname;
-	charset=UTF8;
-	dbname=$database",
-	$username,
-	$password
-);
-//Handles possible database connection error
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// overwrite with real database credentials
-if(file_exists(__DIR__.'/secret.pdo.php')){
-	require __DIR__.'/secret.pdo.php';
-}
 // get data and store in a json array
 $query = "SELECT GearID, GearType FROM Gear";
-$result = $pdo-->prepare($query);
+$result = $pdo->prepare($query);
 $result->execute();
 /* bind result variables */
 $result->bind_result($GearID, $GearType);
